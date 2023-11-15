1044c8a341d6f3304c4ecb61eab5293f9dd58a11
aqib0770 <aqibansari72a@gmail.com>
2023-11-15 20:19:24 +0530

    Changes in application

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 from flask import Flask,request,render_template,jsonify
 from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
+from src.exception import CustomException
+from src.logger import logging
+import sys
 
 application=Flask(__name__)
 
@@ -9,28 +12,32 @@ def home_page():
 
 @application.route('/predict',methods=['POST','GET'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('form.html')
-    else:
-        data=CustomData(
-            carat=float(request.form.get('carat')),
-            depth=float(request.form.get('depth')),
-            table=float(request.form.get('table')),
-            x=float(request.form.get('x')),
-            y=float(request.form.get('y')),
-            z=float(request.form.get('z')),
-            cut=request.form.get('cut'),
-            color=request.form.get('color'),
-            clarity=request.form.get('clarity')
-            )
-        
-        final_new_data=data.get_data_as_dataframe()
-        predict_pipeline=PredictPipeline()
-        pred=predict_pipeline.predict(final_new_data)
+    try:
+        if request.method=='GET':
+            return render_template('form.html')
+        else:
+            data=CustomData(
+                carat=float(request.form.get('carat')),
+                depth=float(request.form.get('depth')),
+                table=float(request.form.get('table')),
+                x=float(request.form.get('x')),
+                y=float(request.form.get('y')),
+                z=float(request.form.get('z')),
+                cut=request.form.get('cut'),
+                color=request.form.get('color'),
+                clarity=request.form.get('clarity')
+                )
 
-        results=round(pred[0],2)
-        
-        return render_template('form.html',final_result=results)
-    
+            final_new_data=data.get_data_as_dataframe()
+            predict_pipeline=PredictPipeline()
+            pred=predict_pipeline.predict(final_new_data)
+
+            results=round(pred[0],2)
+
+            return render_template('form.html',final_result=results)
+    except Exception as e:
+        logging.info('Exception occured at Data Ingestion Stage')
+        raise CustomException(e,sys)
+
 if __name__=='__main__':
     application.run(host="0.0.0.0",debug=True)
